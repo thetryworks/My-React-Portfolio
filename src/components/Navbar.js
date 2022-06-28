ee2c4ddb5a74398f0bbe3265a982158b29d7a9e3
thetryworks <natesmyth1@gmail.com>
2022-06-28 14:50:40 -0500

    styling and creating the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,25 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <nav className="navbar navbar-expand-lg bg-dark">
+  <div className="container">
+    <a className="navbar-brand" href="#">Nathaniel Hernandez</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="#">About Me</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#">Portfolio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Resume</a>
         </li>
         </ul>
       
