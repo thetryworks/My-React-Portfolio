f78039c82f6386a290fcc1b1d4dd16b73bada230
thetryworks <natesmyth1@gmail.com>
2022-06-29 10:52:22 -0500

    added more navbar styling and functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,33 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark">
-  <div className="container">
-    <a className="navbar-brand" href="#">Nathaniel Hernandez</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">About Me</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Portfolio</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Resume</a>
-        </li>
-        </ul>
-      
-    </div>
-  </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">Nathaniel Hernandez</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <FontAwesomeIcon icon={ faBars } style={{ color: "#fff" }}/>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">About Me</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Portfolio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Resume</a>
+            </li>
+            </ul>
+          
+        </div>
+      </div>
 </nav>
   )
 }
