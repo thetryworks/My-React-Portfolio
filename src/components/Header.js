771800aa521687cb3ef757ea9bff81a36f704643
thetryworks <natesmyth1@gmail.com>
2022-07-04 09:05:39 -0500

    slight style changes:

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,13 @@ import React from 'react'
 export const Header = () => {
   return (
     <div className="header-wrapper">
-        <div className="main-info">
+        <div className="container">
+          <div className="row">
+            <div className="header-text col-12">
             <h1>Hi! I'm Nathaniel</h1>
                 <p>Web Developer</p>
+            </div>
+          </div>
         </div>
     </div>
   )
